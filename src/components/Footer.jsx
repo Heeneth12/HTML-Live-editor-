f31d3eab0daa6f46236815581e3459a4903fabf1
heeneth <heeneth123@gmail.com>
2024-02-24 17:37:03 +0530

    added privacy and policy like in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,8 @@ function FotterBar() {
     <>
       <footer class="bg-gray-800 text-white py-1">
         <div class="container mx-auto flex items-center justify-center">
-          <p>&copy; 2023 Live Style. All rights reserved.</p>
+          <p>&copy; 2024 Live Style. All rights reserved.</p>
+          <a href="/Privacy-Policy">Privacy & Policy</a>
         </div>
       </footer>
     </>
