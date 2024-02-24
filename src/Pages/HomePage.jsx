2490438ceac139eff59fea8d6710b4eeb695cbb9
heeneth <heeneth123@gmail.com>
2024-02-24 14:34:45 +0530

    added navbar , footer and code editer

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+import CodeEditer from "../components/CodeEditer";
+import Footer from "../components/Footer";
+
+function HomePage() {
+  return (
+    <>
+      <NavBar />
+      <CodeEditer />
+      <Footer />
+    </>
+  );
+}
+
+export default HomePage;
